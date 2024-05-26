3d85af8a224d9b7f711df44f3982a57294c499cf
carlos <cpinilla144@gmail.com>
2024-05-26 12:32:17 -0400

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const apikey = "b8d103e568964d4284df60301dff7187";
+const apiKey = "b8d103e568964d4284df60301dff7187";
 
 const blogContainer = document.getElementById("bolg-container");
+const searchField = document.getElementById('search-input')
+const searchButton = document.getElementById('search-button')
 
 async function fetchRandomNews() {
   try {
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apikey}`;
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apiKey}`;
     const response = await fetch(apiUrl)
     const data = await response.json()
    return data.articles;
@@ -13,6 +15,36 @@ async function fetchRandomNews() {
     return [];
   }
 }
+
+searchButton.addEventListener("click",async ()=>{
+    const query = searchField.value.trim
+    ()
+    if(query !==""){
+        try{
+const articles = await fetchNewsQuery
+(query)
+displayBlogs(articles)
+        }catch(error){
+            console.log("error fetching news by query", error)
+
+        }
+    }
+})
+
+async function fetchNewsQuery(query){
+    try {
+        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apikey=${apiKey}`;
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+       return data.articles;
+      } catch (error) {
+        console.error("error fetching random news", error);
+        return [];
+      }
+    }
+
+
+
 
 function displayBlogs(articles){
     blogContainer.innerHTML = ""
@@ -23,22 +55,32 @@ const img = document.createElement("img")
 img.src = article.urlToImage
 img.alt = article.title
 const title = document.createElement ("h2")
-title.textContent = article.title
+const truncatedTitle =article.title.length
+> 30? article.title.slice(0,30) + "....":
+article.title;
+title.textContent = truncatedTitle;
 const description = document.createElement
 ("p")
-description.textContent = article.add
-description
-blogCard.appendChild(img)
-blogCard.appendChild(img)
-blogCard.appendChild(img)
+const truncatedDes = article.description.length
+> 120? article.description.slice(0,120) + "....":
+article.description;
+description.textContent = truncatedDes;
+description;
+blogCard.appendChild(img);
+blogCard.appendChild(title);
+blogCard.appendChild(description);
+blogCard.addEventListener("click",() => {
+    window.open(article.url, "_blank" );
+});
+blogContainer.appendChild(blogCard);
     })
 }
 
-async ()=>{
+(async ()=>{
     try {
        const articles = await fetchRandomNews();
        displayBlogs(articles);
     } catch(error){
 console.error ("error fetching random news", error);
     }
-};
+})();
